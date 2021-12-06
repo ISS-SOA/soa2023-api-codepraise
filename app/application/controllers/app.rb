5d6b989f13c4fb983d2a923eb6f568470fd507dd
Soumya Ray <soumya.ray@gmail.com>
2021-12-06 08:03:57 +0800

    reverse-proxy caching of project appraisal

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -6,12 +6,11 @@ module CodePraise
   # Web App
   class App < Roda
     plugin :halt
-    plugin :flash
+    plugin :caching
     plugin :all_verbs # allows DELETE and other HTTP verbs beyond GET/POST
 
     # use Rack::MethodOverride # for other HTTP verbs (with plugin all_verbs)
 
-    # rubocop:disable Metrics/BlockLength
     route do |routing|
       response['Content-Type'] = 'application/json'
 
@@ -32,6 +31,8 @@ module CodePraise
           routing.on String, String do |owner_name, project_name|
             # GET /projects/{owner_name}/{project_name}[/folder_namepath/]
             routing.get do
+              response.cache_control public: true, max_age: 300
+
               path_request = Request::ProjectPath.new(
                 owner_name, project_name, request
               )
@@ -87,6 +88,5 @@ module CodePraise
         end
       end
     end
-    # rubocop:enable Metrics/BlockLength
   end
 end
